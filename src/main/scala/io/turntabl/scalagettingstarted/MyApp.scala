7267f91827eb7792a186a6fd6d9fef0ba36fe7de
bill <teebill15@gmail.com>
2019-11-25 12:31:45 +0100

    added more files

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -2,12 +2,22 @@ package io.turntabl.scalagettingstarted
 
 object MyApp extends App {
 
-//    Printing out my name and my hometown
-    val fName:String = "Bill Ali"
-    val homeTown:String = "Siniko"
-    print(s"$fName was born in $homeTown in the Upper East Region of Ghana" )
+    ////    Printing out my name and my hometown
+    //    val fName:String = "Bill Ali"
+    //    val homeTown:String = "Siniko"
+    //    print(s"$fName was born in $homeTown in the Upper East Region of Ghana" )
 
-//    Writing a method called threeTimes that takes a string and returns the string three times.
+    //    Writing a method called threeTimes that takes a string and returns the string
+    //even function
+//    def even(i:Int) = if (i % 2==0)
+
+    def wordsToLower(words: String*) =words map (_.toLowerCase())
+    def even2_?(i: Int) = i%2 ==0
+
 
 
 }
+
+
+
+
