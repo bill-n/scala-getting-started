728ff9a3a39cd09e7703811595d06fb7c49b8d21
bill <teebill15@gmail.com>
2019-11-25 11:22:44 +0100

    lab 1

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -1,5 +1,13 @@
 package io.turntabl.scalagettingstarted
 
 object MyApp extends App {
-    print("Hello, World!")
+
+//    Printing out my name and my hometown
+    val fName:String = "Bill Ali"
+    val homeTown:String = "Siniko"
+    print(s"$fName was born in $homeTown in the Upper East Region of Ghana" )
+
+//    Writing a method called threeTimes that takes a string and returns the string three times.
+
+
 }
